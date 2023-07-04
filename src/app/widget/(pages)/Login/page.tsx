354d200b528ecfde61c163e55ba589d2f26d3d91
Bhagavan Marpadaga <bhagavan.m@lsq.net>
2023-07-04 15:04:09 +0530

    updated widget login ui

--- src/app/widget/(pages)/Login/page.tsx
+++ src/app/widget/(pages)/Login/page.tsx
@@ -3,7 +3,13 @@ import { FormButton } from '@/components/authForm/FormButton';
 import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { Card } from '@/components/ui/card';
-import { LOGO } from '@/constants';
+import {
+  LOGO,
+  GOOGLEICON,
+  APPLEICON,
+  GITHUBICON,
+  MICROSOFT
+} from '@/constants';
 import Image from 'next/image';
 import React from 'react';
 
@@ -12,8 +18,8 @@ const Login = () => {
     console.log('submit called');
   };
   return (
-    <div className="w-full flex justify-center items-center absolute top-1/4 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/5 md:top-1/2 ">
-      <div className="flex flex-col items-center w-11/12 mr-0 !important bg-white border-2 border-gray-300 rounded-2xl shadow-2xl">
+    <div className="w-full top-1/2 flex justify-center items-center absolute  left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-2/5  ">
+      <div className="flex flex-col items-center w-full mr-0 !important bg-white border-2 border-gray-300 rounded-2xl shadow-2xl sm:w-8/12">
         <div className="flex flex-col justify-center items-center mt-7">
           <Image width={34} height={34} src={LOGO} alt="AuthX logo" />
           <div className="mt-4">
@@ -28,9 +34,9 @@ const Login = () => {
 
         <form
           onSubmit={handleSubmit}
-          className="mt-10 flex flex-col items-center"
+          className={`mt-10 flex flex-col items-center w-9/12 `}
         >
-          <div>
+          <div className="grid grid-cols-1 w-full">
             <label
               htmlFor="email"
               className={`absolute translate-x-6 translate-y-[-12px] bg-white px-1`}
@@ -40,18 +46,24 @@ const Login = () => {
             <input
               id="email"
               type="text"
-              className="border border-red-500 bg-white text-gray-900 rounded-xl focus:ring-gray-900 focus:border-gray-900 w-full"
+              className=" p-2 pl-2.5 border-2 border-gray-500 bg-white text-gray-900 rounded-lg focus:ring-gray-900 focus:border-gray-900 "
               placeholder="name@example.com"
             />
           </div>
           <div className="w-full mt-0">
-            <FormButton>{`GO `}</FormButton>
+            <FormButton padding={1} marginTop={8}>{`GO `}</FormButton>
           </div>
         </form>
-        <div className="flex w-11/12 justify-center">
+        <div className="flex w-11/12 justify-center mt-4">
           <div className="mt-4 w-10/12 border-t-2 border-gray-900 "></div>
           <span className="p-1"> or </span>
           <div className="mt-4 w-10/12 border-t-2 border-gray-900 "></div>
+        </div>
+        <div className="flex my-10 w-8/12 justify-around">
+          <Image width={25} height={25} src={GOOGLEICON} alt="AuthX logo" />
+          <Image width={30} height={30} src={APPLEICON} alt="AuthX logo" />
+          <Image width={30} height={30} src={GITHUBICON} alt="AuthX logo" />
+          <Image width={30} height={30} src={MICROSOFT} alt="AuthX logo" />
         </div>
       </div>
     </div>
--- src/components/authForm/FormButton.tsx
+++ src/components/authForm/FormButton.tsx
@@ -1,16 +1,31 @@
-import { FaAngleRight } from "react-icons/fa";
-import { createRipple } from "@/helper/createRipple";
+import { FaAngleRight } from 'react-icons/fa';
+import { createRipple } from '@/helper/createRipple';
 
-export const FormButton = ({ children }: { children: React.ReactNode }) => {
+export const FormButton = ({
+  children,
+  marginTop,
+  padding
+}: {
+  children: React.ReactNode;
+  marginTop?: number;
+  padding?: number;
+}) => {
+  console.log('magin top i s', marginTop);
   return (
     <button
       onClick={createRipple}
       type="submit"
-      className="group relative overflow-hidden w-full text-white hover:shadow-lg rounded-lg mt-8 py-3 md:mt-11 btn-ca bg-gradient-to-r from-black to-[#6F6F6F] flex items-center justify-center"
+      className={`group relative overflow-hidden w-full text-white hover:shadow-lg rounded-lg mt-${
+        marginTop ? marginTop : 8
+      } py-${padding === 0 || padding ? padding : 3} md:mt-${
+        marginTop ? marginTop : 8
+      } btn-ca bg-gradient-to-r from-black to-[#6F6F6F] flex items-center justify-center`}
     >
-      <span className="text-2xl font-semibold tracking-[0.25em]">{children}</span>
+      <span className="text-2xl font-semibold tracking-[0.25em]">
+        {children}
+      </span>
       <span className="group-hover:translate-x-3 ease-in duration-300">
-        {" "}
+        {' '}
         <FaAngleRight className="text-2xl mt-[2px]" />
       </span>
     </button>
