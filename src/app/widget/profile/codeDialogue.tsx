c315858cee8b428ce4914f2b904e4858f06747a5
piyush yadav <yadavpiyush222@gmail.com>
2023-09-14 18:02:10 +0530

    resolved token issue in widget/profile

--- src/app/widget/profile/codeDialogue.tsx
+++ src/app/widget/profile/codeDialogue.tsx
@@ -6,38 +6,28 @@ import { useState } from 'react';
 import Spinner from '@/components/spinner';
 
 import { DialogContent } from '@/components/ui/Dialog';
+import { Label } from '@/components/ui/Label';
 
-// export type PasswordDialogueProp = {
-//   request: (pass: string) => Promise<void>;
-//   loading: boolean;
-// };
-export function CodeDialogue() {
-  const [code, setCode] = useState('');
-  const [loading, setLoading] = useState(false)
+export type PasswordDialogueProp = {
+  code: string;
+};
+export function CodeDialogue({ code }: PasswordDialogueProp) {
+  const [loading, setLoading] = useState(false);
   return (
     <DialogContent>
       <div className="flex flex-col items-center py-4 sm:px-10 gap-y-5">
-        <label className="mb-2" htmlFor="password">
-          Please enter the code from the authenticator app
-        </label>
-        <Input
-          id="code"
-          type="text"
-          value={code}
-          placeholder="code"
-          onChange={e => setCode(e.target.value)}
-        />
-        <Button
-          variant="black"
-          className="w-[240px] mt-4 "
-        >
+        <Label className="mb-2" htmlFor="code">
+          Use the code
+        </Label>
+        <Input id="code" type="text" value={code} />
+        <Button variant="black" className="w-[240px] mt-4 ">
           {loading ? (
             <div className="flex flex-row gap-2 items-center">
               <Spinner size={16} color="gray" />
               <span>Processing...</span>
             </div>
           ) : (
-            'Proceed'
+            'Done'
           )}
         </Button>
       </div>
